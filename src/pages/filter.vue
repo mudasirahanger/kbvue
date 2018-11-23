e8a1ac4f2e1c11660f5ac24fc4802c8fddf9f2dc
Sameerkashmirbox <sameer.kashmirbox@gmail.com>
2018-11-23 12:37:30 -0800

    Filter

--- src/pages/filter.vue
+++ src/pages/filter.vue
@@ -1,7 +1,10 @@
       <template>
         <f7-page on:Load="openIndicator" >
          <!-- <f7-navbar title="Back"> <Link back>Back</Link> </f7-navbar>-->   
-          <f7-block strong>Filter</f7-block> 
+            
+            <f7-navbar back-link="Back" > Back</f7-navbar> 
+            <f7-block strong><h1 style="text-align:Center"> Filter By</h1></f7-block>
+           
            <!--<f7-block>
             <f7-searchbar placeholder="search"></f7-searchbar>
           </f7-block>-->
@@ -102,8 +105,19 @@
             </f7-col>
             <f7-col>
               <f7-block>
-                  <f7-block-title>Filter by </f7-block-title>
-
+                  
+                      <f7-card>
+                        <f7-card-header> <f7-label ><f7-block-title><h1>Filter by</h1> </f7-block-title></f7-label>  </f7-card-header>
+                   <f7-card-content> 
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, architecto. Cupiditate laudantium rem nesciunt numquam, ipsam. Voluptates omnis, a inventore atque ratione aliquam. Omnis iusto nemo quos ullam obcaecati, quod.
+                   </f7-card-content>
+                    
+                   <f7-card-footer>
+                    <f7-link icon-if-ios="f7:filter-fill"  back-link="Back" icon-if-md="material:cancel"> cancel</f7-link>
+                    
+                   </f7-card-footer>
+                   
+                 </f7-card>
               </f7-block>
             </f7-col>
             </f7-col>
